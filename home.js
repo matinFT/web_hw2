991794beb76521a2dc7849be865721dbe55fb6f7
mohammad matin fotouhi <matin.fotouhi@yahoo.com>
2020-11-23 12:13:37 +0330

    fixed some bugs in home page

--- home.js
+++ home.js
@@ -3,7 +3,7 @@ sidebar_opened = false
 function change_sidebar() {
     document.getElementById("sidenav-button").classList.toggle("sidenav-opened")
     if (sidebar_opened) {
-        document.getElementById("sidebar").style.width = "0px";
+        document.getElementById("sidebar").style.removeProperty('width');
         sidebar_opened = false
     }
     else {
